bc5c6c49bab12a2013078fc8b5abd4564266563f
nicovanbentum <niico0708@gmail.com>
2019-06-17 11:56:09 +0200

    fixed review remarks

--- test/test_video_feed.py
+++ test/test_video_feed.py
@@ -1,13 +1,16 @@
 """ Tests the behavior of the VideoFeed class. """
 
-import pytest
-
 from modules.vision.module.video_feed import VideoFeedCV2
 
 
 class TestVideoFeedCV2:
     """ Groups the tests for the VideoFeedCV2 class,
     please connect at least one camera. """
+
+    def test_open(self):
+        """ Tests if the device is correctly opened (requires a connected camera). """
+        vf = VideoFeedCV2(0)
+        assert(vf.device.isOpened())
 
     def test_resolution(self):
         """ Tests if we are able to get and set the resolution correctly. """
@@ -17,11 +20,6 @@ class TestVideoFeedCV2:
         res = vf.get_resolution()
         assert(res[0] == w)
         assert(res[1] == h)
-
-    def test_open(self):
-        """ Tests if the device is correctly opened (requires a connected camera). """
-        vf = VideoFeedCV2(0)
-        assert(vf.device.isOpened())
 
     def test_detach(self):
         """ Tests if the device is correctly released (requires a connected camera). """
