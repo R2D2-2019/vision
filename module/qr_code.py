62e79acec4f63fc32e171a431f7f1f4a6e9378b5
BasvRossem <31800724+BasvRossem@users.noreply.github.com>
2019-06-18 14:24:46 +0200

    Added documentation and fixes minor inconsistencies.

--- module/qr_code.py
+++ module/qr_code.py
@@ -1,28 +1,55 @@
+""" Provides a class in which to save qr code data."""
+
+
 class QrCode:
+    """ This class is used to store qr code data and do some simple calculations."""
+
     def __init__(self, code):
-        self.data = self.parse_data(code.data.decode("utf-8"))
+        """ The constructor.
+        :param code: A pyzbar decoded qrcode.
+        """
+        self.data = None
         self.type = code.type
         self.rect = code.rect
         self.polygon = code.polygon
         self.distance = None
+        self.parse_data(code.data.decode("utf-8"))
 
     def calculate_distance(self, camera_properties, frame_height):
+        """ Calculate the distance between the qrcode and the camera.
+        :param camera_properties: Properties of the camera used in making the frame/picture.
+        :param frame_height: Height of the frame/picture in which the qrcode is found.
+        :return: Nothing.
+        """
         try:
-            distance_to_object = (camera_properties.get_focal_length() * self.get_height() * frame_height) / (self.rect.height * camera_properties.get_sensor_height())
+            distance_to_object = (camera_properties.get_focal_length() * self.get_height(
+            ) * frame_height) / (self.rect.height * camera_properties.get_sensor_height())
             self.distance = distance_to_object
+        # Sometimes something goes wrong in calculating the distance
+        # resulting in an ZeroDivisionError.
+        # If this is the case nothing should happen.
+        # Else the program will crash.
         except ZeroDivisionError:
-            return None
+            self.distance = None
 
     def get_height(self):
+        """ Gets height measurement of the qr code.
+        This height value is stored inside of the qr code itself.
+        :return: Qr code height.
+        """
         try:
             return int(self.data["Height"])
         except ValueError:
             return None
 
     def parse_data(self, unrefined_data):
+        """ Parses the data of the qr code
+        :param unrefined_data: A string of data to parse.
+        :return: Nothing.
+        """
         data_dict = dict()
         data_splits = unrefined_data.split(",")
         for data_split in data_splits:
             data = data_split.split(":")
             data_dict[data[0]] = data[1]
-        return data_dict
+        self.data = data_dict
--- module/qr_reader.py
+++ module/qr_reader.py
@@ -1,12 +1,24 @@
-from qr_code import QrCode
+"""Provedes an interface to get qrcodes from an image"""
+
 from pyzbar import pyzbar
+from qr_code import QrCode
 
 
 class QrReader:
+    """ The QrReader class reads the qr codes from a frame"""
+
     def __init__(self):
+        """ The contstructor.
+        It creates a list for the qrcodes to be put into.
+        """
         self.codes = list()
 
     def read_qr_codes(self, frame, camera_properties=None):
+        """ Reads the qrcodes in the frame.
+        :param frame: The image from which the qrcodes must be read.
+        :param camera_properties: The properties of the camera.  
+        :return: Nothing.
+        """
         codes = pyzbar.decode(frame)
 
         # Create the qrcodes
@@ -15,10 +27,12 @@ class QrReader:
         # Calculate distance if possible
         if camera_properties:
             for code in self.codes:
-                print("Frame height: {}".format(frame.shape[0]))
                 code.calculate_distance(camera_properties, frame.shape[0])
 
     def get_qr_codes(self):
+        """ Gets the qrdoes found in the frame.
+        :return: Returns a list of found qr codes.
+        """
         if self.codes:
             return self.codes
         return None
