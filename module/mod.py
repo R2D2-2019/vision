36a673f2df10f36ee5ee93e8fb3f1490e0b0c97a
Nico van Bentum <31741718+nicovanbentum@users.noreply.github.com>
2019-06-19 19:56:09 +0200

    docs for module file

--- module/mod.py
+++ module/mod.py
@@ -1,7 +1,6 @@
+""" Python module for the Vision module. """
 from client.comm import BaseComm
 from common.frame_enum import FrameType
-
-import cv2
 
 # TODO: make a PR for this frame type
 
@@ -17,6 +16,9 @@ class FRAMETYPE:
 
 
 class Vision:
+    """ This is the main module class used for every step in the process. 
+    See class/activity diagrams for more.  """
+
     def __init__(self, comm: BaseComm, video_feed, qr_reader, camera_properties):
         self.comm = comm
         self.video_feed = video_feed
@@ -25,17 +27,19 @@ class Vision:
         # self.comm.listen_for([FrameType.BUTTON_STATE])
 
     def reset_video_feed(self, feed):
+        """ Use this function to attach a new video feed. """
         self.video_feed = feed
 
     def reset_reader(self, new_reader):
+        """ Use this function to attach a new QR reader. """
         self.qr_reader = new_reader
 
     def process(self):
+        """ The process function that's provided by the template. This generates QR
+        codes from a video frame and puts them on the bus by request. """
         # self.comm.send(FrameType.BUTTON_STATE, (1,2,3))
 
         video_frame = self.video_feed.get_frame()
-        cv2.imshow("frame", video_frame)
-        cv2.waitKey(1)
         self.qr_reader.read_qr_codes(video_frame, self.cp)
 
         frames = list()
@@ -63,4 +67,5 @@ class Vision:
             print(self.comm.get_data())
 
     def stop(self):
+        """ Stop function provided by the template. """
         self.comm.stop()
