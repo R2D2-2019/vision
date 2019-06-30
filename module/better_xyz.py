4a09df093bd1081a41eb20a663901ebb074b6620
BasvRossem <31800724+BasvRossem@users.noreply.github.com>
2019-06-30 11:44:09 +0200

    Fixed perspective issues and code cleanup

--- module/better_xyz.py
+++ module/better_xyz.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 def line_intersection(line_1, line_2):
     x_diff = (line_1[0][0] - line_1[1][0], line_2[0][0] - line_2[1][0])
     y_diff = (line_1[0][1] - line_1[1][1], line_2[0][1] - line_2[1][1])
@@ -14,143 +16,59 @@ def line_intersection(line_1, line_2):
     y = det(d, y_diff) / div
     return [x, y]
 
-# # Get center point of a plane in 3d space
-# polygon = [[190,104],[721,342],[189,707],[723,460]]
-# slanted_line_1 = [polygon[0], polygon[3]]
-# slanted_line_2 = [polygon[1], polygon[2]]
-# print(line_intersection(slanted_line_1, slanted_line_2))
-
-# print("==================================================")
-
-# # Get center height of a plane in 3d space
-# import copy
-# polygon_2 = [[969,236],[1403,89],[969,489],[1403,671]]
-
-# slanted_line_1_2 = [polygon_2[0], polygon_2[3]]
-# slanted_line_2_2 = [polygon_2[1], polygon_2[2]]
-
-# upper_line_2 = [polygon_2[0], polygon_2[1]]
-# lower_line_2 = [polygon_2[2], polygon_2[3]]
-# left_line_2 = [polygon_2[0],polygon_2[2]]
-# right_line_2 = [polygon_2[1],polygon_2[3]]
-
-# left_line_height = left_line_2[1][1] - left_line_2[0][1], 
-# right_line_height = right_line_2[1][1] - right_line_2[0][1],
-
-# longer_line = [[0]*2]*2
-# longer_line[0] = left_line_2[0].copy()
-# longer_line[1] = left_line_2[1].copy()
-
-# if left_line_height > right_line_height:
-#     longer_line[0] = left_line_2[0].copy()
-#     longer_line[1] = left_line_2[1].copy()
-# else:
-#     longer_line[0] = right_line_2[0].copy()
-#     longer_line[1] = right_line_2[1].copy()
-
-
-# # Move longest line to the middle
-# center = line_intersection(slanted_line_1_2, slanted_line_2_2)
-# longer_line_center = [(longer_line[0][0] + longer_line[1][0]) / 2, (longer_line[0][1] + longer_line[1][1]) / 2]
-
-# line_diff = center[0] - longer_line_center[0]
-
-# print(polygon_2)
-# print(type(left_line_2[0]))
-# longer_line[0][0] = longer_line[0][0] + line_diff
-# longer_line[1][0] = longer_line[1][0] + line_diff
-
-# print(polygon_2)
-
-
-# intersect_1 = line_intersection(longer_line, upper_line_2)
-# intersect_2 = line_intersection(longer_line, lower_line_2)
-
-
-# height = intersect_2[1] - intersect_1[1]
-
-# print(height)
-
-print("=============================================")
-from math import sqrt
-import time
-import timeit
 # All the lines
-#def polygon_measure_toolkit():
-polygon = [[512, 565],[999,531],[782,867],[1229,651]]
+def calculate_polygon_properties(polygon):
+    diagonal_lt_rb_line = [polygon[0], polygon[3]]
+    diagonal_rt_lb_line = [polygon[1], polygon[2]]
 
-slanted_line_1 = [polygon[0], polygon[3]]
-slanted_line_2 = [polygon[1], polygon[2]]
+    top_line = [polygon[0], polygon[1]]
+    bottom_line = [polygon[2], polygon[3]]
+    left_line = [polygon[0], polygon[2]]
+    right_line = [polygon[1], polygon[3]]
 
-upper_line_3 = [polygon[0], polygon[1]]
-lower_line_3 = [polygon[2], polygon[3]]
-left_line_3 = [polygon[0], polygon[2]]
-right_line_3 = [polygon[1], polygon[3]]
+    center_point = line_intersection(diagonal_lt_rb_line, diagonal_rt_lb_line)
 
-center = line_intersection(slanted_line_1, slanted_line_2)
+    horizontal_vanish_point = line_intersection(top_line, bottom_line) 
+    vertical_vanish_point = line_intersection(left_line, right_line) 
 
-upper_lower_vanish = line_intersection(upper_line_3, lower_line_3) 
-left_right_vanish = line_intersection(left_line_3, right_line_3) 
+    horizontal_center_line = [center_point, [center_point[0] + 10, center_point[1]]] 
+    vertical_center_line = [center_point, [center_point[0], center_point[1] + 10]]
 
-line_through_center_horizontal = [[0]*2]*2 
-line_through_center_vertical = [[0]*2]*2
+    if horizontal_vanish_point:
+        horizontal_center_line = [center_point, horizontal_vanish_point]
+    if vertical_vanish_point:
+        vertical_center_line = [center_point, vertical_vanish_point]
 
-if upper_lower_vanish:
-    line_through_center_horizontal = [center, upper_lower_vanish]
-if left_right_vanish:
-    line_through_center_vertical = [center, left_right_vanish]
+    upper_middle_point = line_intersection(vertical_center_line, top_line)
+    lower_middle_point = line_intersection(vertical_center_line, bottom_line)
+    left_middle_point = line_intersection(horizontal_center_line, left_line)
+    right_middle_point = line_intersection(horizontal_center_line, right_line)
 
-upper_middle_point = line_intersection(line_through_center_vertical, upper_line_3)
-lower_middle_point = line_intersection(line_through_center_vertical, lower_line_3)
-left_middle_point = line_intersection(line_through_center_horizontal, left_line_3)
-right_middle_point = line_intersection(line_through_center_horizontal, right_line_3)
+    height_line = [upper_middle_point, lower_middle_point] 
+    width_line = [left_middle_point, right_middle_point]
 
-height_line = [upper_middle_point, lower_middle_point] 
-width_line = [left_middle_point, right_middle_point]
+    height = sqrt(((height_line[0][0] - height_line[1][0])**2) + ((height_line[0][1] - height_line[1][1])**2))
+    width = sqrt((width_line[0][0] - width_line[1][0])**2 + (width_line[0][1] - width_line[1][1])**2)
 
-height = sqrt(((height_line[0][0] - height_line[1][0])**2) + ((height_line[0][1] - height_line[1][1])**2))
-width = sqrt((width_line[0][0] - width_line[1][0])**2 + (width_line[0][1] - width_line[1][1])**2)
+    print(polygon)
+    print(center_point)
+    print(upper_middle_point)
+    print(lower_middle_point)
+    print(left_middle_point)
+    print(right_middle_point)
+    print(height_line)
+    print(width_line)
+    print(height)
+    print(width)
 
-# loop_amount = 1000000
-# print("time in s: {}".format(timeit.timeit(polygon_measure_toolkit, number = loop_amount)))
-# print(height_line)
-# print(width_line)
-# print(height)
-# print(width)
+polygon = [[500,500],[700,500],[500,700],[700,700]] # square
+polygon = [[500,500],[700,550],[500,700],[700,650]] #vanish point right
+polygon = [[500,550],[700,500],[500,650],[700,700]] #vanish point left
+polygon = [[550,500],[650,500],[500,700],[700,700]] #vanish point up
+polygon = [[500,500],[700,500],[550,700],[650,700]] #vanish point down
+polygon = [[512, 565],[999,531],[782,867],[1229,651]] # vanish point yes
 
-# print(start)
-# print(stop)
-# print("Time it took to calculate evrything: {}".format(start - stop))
-# Show how awesome it is
-import cv2
-import numpy as np
+calculate_polygon_properties(polygon)
 
-frame = np.zeros((1080, 1920, 3), np.uint8)
-while True:
-    cv2.imshow("preview", frame)
- 
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
-    else:
-        # Vanishing points
-        cv2.line(img=frame, pt1=(polygon[0][0], polygon[0][1]), pt2=(int(left_right_vanish[0]), int(left_right_vanish[1])), color=(255,255,255), thickness=5, lineType=8, shift=0)
-        cv2.line(img=frame, pt1=(polygon[1][0], polygon[1][1]), pt2=(int(left_right_vanish[0]), int(left_right_vanish[1])), color=(255,255,255), thickness=5, lineType=8, shift=0)
-        cv2.line(img=frame, pt1=(polygon[1][0], polygon[1][1]), pt2=(int(upper_lower_vanish[0]), int(upper_lower_vanish[1])), color=(255,255,255), thickness=5, lineType=8, shift=0)
-        cv2.line(img=frame, pt1=(polygon[3][0], polygon[3][1]), pt2=(int(upper_lower_vanish[0]), int(upper_lower_vanish[1])), color=(255,255,255), thickness=5, lineType=8, shift=0)
-
-        # Diagonal
-        cv2.line(img=frame, pt1=(polygon[0][0], polygon[0][1]), pt2=(polygon[3][0], polygon[3][1]), color=(0, 128, 255), thickness=5, lineType=8, shift=0)
-        cv2.line(img=frame, pt1=(polygon[1][0], polygon[1][1]), pt2=(polygon[2][0], polygon[2][1]), color=(0, 128, 255), thickness=5, lineType=8, shift=0)
-        
-        # Upper lower left right
-        cv2.line(img=frame, pt1=(polygon[0][0], polygon[0][1]), pt2=(polygon[1][0], polygon[1][1]), color=(255, 0, 0), thickness=5, lineType=8, shift=0)
-        cv2.line(img=frame, pt1=(polygon[2][0], polygon[2][1]), pt2=(polygon[3][0], polygon[3][1]), color=(255, 0, 0), thickness=5, lineType=8, shift=0)
-        cv2.line(img=frame, pt1=(polygon[0][0], polygon[0][1]), pt2=(polygon[2][0], polygon[2][1]), color=(255, 0, 0), thickness=5, lineType=8, shift=0)
-        cv2.line(img=frame, pt1=(polygon[1][0], polygon[1][1]), pt2=(polygon[3][0], polygon[3][1]), color=(255, 0, 0), thickness=5, lineType=8, shift=0)
-
-        # Midlines
-        cv2.line(img=frame, pt1=(int(height_line[0][0]), int(height_line[0][1])), pt2=(int(height_line[1][0]), int(height_line[1][1])), color=(0, 204, 204), thickness=5, lineType=8, shift=0)
-        cv2.line(img=frame, pt1=(int(width_line[0][0]), int(width_line[0][1])), pt2=(int(width_line[1][0]), int(width_line[1][1])), color=(0, 204, 204), thickness=5, lineType=8, shift=0)
-
-
+# TODO : Make polygon class so that he values associated with the polygon are contained within the polygon
+# TODO : Make the function add the calculated values to the custom polygon class
